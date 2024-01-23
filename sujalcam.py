c3b7310a8df2592201c0aff4aeae871978378a75
Sujall10 <94795609+Sujall10@users.noreply.github.com>
2024-01-24 01:09:58 +0530

    changes

---
+++ sujalcam.py
@@ -0,0 +1,19 @@
+import cv2
+face_cap=cv2.CascadeClassifier("C:/Users/SURAJ/AppData/Local/Programs/Python/Python312/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+video_cap  = cv2.VideoCapture(0)
+while True:
+    ret , video_data=video_cap.read()
+    col = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("Video_Live",video_data)
+    if cv2.waitKey(10) == ord("a"):
+        break
+video_cap.release()
